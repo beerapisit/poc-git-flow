ac1b3618150d16d576b0c1bda9e85f0036bddf4e
Apisit <Kaewfoy>
2022-01-07 14:34:42 +0700

    feature3

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+ 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
