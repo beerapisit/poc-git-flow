0ee2036212c78569a794f414146450a8f880efb8
Apisit <Kaewfoy>
2022-01-07 14:34:42 +0700

    feature3

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! Hello feature 2</a>
+          Welcome to <a href="https://nextjs.org">Next.js! Hello feature 3</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
+ 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
