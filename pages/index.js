a8a12493e6628e3b569578fb22b0faaa8ead5fb4
Apisit <Kaewfoy>
2022-01-07 14:59:30 +0700

    feature 4

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! staging2</a>
+          Welcome to <a href="https://nextjs.org">Next.js! feature 4</a>
         </h1>
 
         <p className={styles.description}>
